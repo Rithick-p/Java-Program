b0d162a2e7a4160a9f2acca7e9d1a6322bfc1e98
Rithick-p <prithick.115@gmail.com>
2024-11-12 00:45:30 -0800

    Add files via upload

--- java10/MenuBar_2.java
+++ java10/MenuBar_2.java
@@ -1,3 +1,6 @@
+// Java program to create a menu bar and add menu to
+// it and also add menuitems to menu and also add
+// an event listener to handle the events
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,68 +10,59 @@ import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.input.KeyCombination;
-
+import java.time.LocalDate;
 public class MenuBar_2 extends Application {
+// launch the application
+public void start(Stage s)
+{
+// set title for the stage
+s.setTitle("creating MenuBar");
+// create a menu
+Menu m = new Menu("Menu");
+// create menuitems
+MenuItem m1 = new MenuItem("menu item 1");
+MenuItem m2 = new MenuItem("menu item 2");
+MenuItem m3 = new MenuItem("menu item 3");
+// add menu items to menu
+m.getItems().add(m1);
+m.getItems().add(m2);
+m.getItems().add(m3);
 
-    // launch the application
-    public void start(Stage s) {
-        // set title for the stage
-        s.setTitle("Creating MenuBar");
+// label to display events
+Label l = new Label("\t\t\t\t"+ "no menu item selected");
+// create events for menu items
+// action event
+EventHandler<ActionEvent> event = new
 
-        // create a menu
-        Menu m = new Menu("Menu");
+EventHandler<ActionEvent>() {
 
-        // create menuitems
-        MenuItem m1 = new MenuItem("Menu Item 1");
-        MenuItem m2 = new MenuItem("Menu Item 2");
-        MenuItem m3 = new MenuItem("Menu Item 3");
+public void handle(ActionEvent e)
+{
+l.setText("\t\t\t\t" +
+((MenuItem)e.getSource()).getText() +" selected");
 
-        // add menu items to menu
-        m.getItems().add(m1);
-        m.getItems().add(m2);
-        m.getItems().add(m3);
+}
+};
+// add event
+m1.setOnAction(event);
+m2.setOnAction(event);
+m3.setOnAction(event);
+// create a menubar
+MenuBar mb = new MenuBar();
+// add menu to menubar
+mb.getMenus().add(m);
+// create a VBox
+VBox vb = new VBox(mb, l);
+// create a scene
+Scene sc = new Scene(vb, 500, 300);
+// set the scene
+s.setScene(sc);
+s.show();
+}
+public static void main(String args[])
+{
+// launch the application
+launch(args);
+}
 
-        // label to display events
-        Label l = new Label("\t\t\t\tNo menu item selected");
-
-        // create events for menu items
-        EventHandler<ActionEvent> event = (ActionEvent e) -> {
-            l.setText("\t\t\t\t" + ((MenuItem) e.getSource()).getText() + " selected");
-        };
-
-        // add event handlers to menu items
-        m1.setOnAction(event);
-        m2.setOnAction(event);
-        m3.setOnAction(event);
-
-        // create a menubar
-        MenuBar mb = new MenuBar();
-
-        // add menu to menubar
-        mb.getMenus().add(m);
-
-        // create a VBox to hold the MenuBar and Label
-        VBox vb = new VBox(mb, l);
-
-        // create a scene
-        Scene sc = new Scene(vb, 500, 300);
-
-        // set the scene
-        s.setScene(sc);
-        
-        // Handling window close event properly
-        s.setOnCloseRequest(event1 -> {
-            System.out.println("Application closed");
-            System.exit(0); // Ensures the application closes properly
-        });
-
-        // show the stage
-        s.show();
-    }
-
-    public static void main(String[] args) {
-        // launch the application
-        launch(args);
-    }
 }
